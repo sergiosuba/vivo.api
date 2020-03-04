e6129f4d1132a40553f5d257e523c9f898d412cf
Sergio Suba <sergiosuba@Suba.local>
2020-03-04 10:00:38 -0300

    configuracao inicial dos dos testes

--- src/test/java/com/vivo/HelloWord.java
+++ src/test/java/com/vivo/HelloWord.java
@@ -1,19 +1,18 @@
 package com.vivo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ActiveProfiles("test")
-class VivoApplicationTests {
+public class HelloWord {
 
 	@Test
-	void contextLoads() {
+	public void testHelloWord() {
+		assertEquals(1, 1);
 	}
-
 }
