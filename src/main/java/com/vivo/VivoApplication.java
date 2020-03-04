5fd0d0ef22e81fd49f781fd743fe743e5ede2722
Sergio Suba <sergiosuba@Suba.local>
2020-03-04 09:00:55 -0300

    inicio do projeto

---
+++ src/main/java/com/vivo/VivoApplication.java
@@ -0,0 +1,13 @@
+package com.vivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vivo/VivoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
